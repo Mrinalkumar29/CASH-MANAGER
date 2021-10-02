b7c25f9a3fc61de73a652ffd0b9603db9cb900cb
Mrinalkumar29 <mrinalkumaramw@gmail.com>
2021-10-02 13:00:32 +0530

    file added

---
+++ addingjstomark10.js
@@ -0,0 +1,72 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const nextButton = document.querySelector("#nextBtn");
+const cashGivenSection = document.querySelector('.cash-given-section')
+const notesSection = document.querySelector('.notes-section')
+
+
+const availableNotes = [2000, 500, 200 ,  100, 50 , 20, 10, 5, 1]
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
+    
+    if (billAmount.value > 0) { 
+        
+        if (cashGiven.value >= billAmount.value) { 
+            const amountToBeReturned = cashGiven.value - billAmount.value; 
+        
+            notesSection.style.display = 'block'
+            calculateChange(amountToBeReturned);
+        }
+        
+        
+        else {
+            showMessage(
+                "Cash amount given should be greater than or equal to bill amount"
+            )
+        }
+
+    } else {
+        showMessage("Invalid bill amount");
+    }
+});
+
+ nextButton.addEventListener("click",  ()=> {
+    if (billAmount.value > 0){
+        cashGivenSection.style.display = 'block'
+        message.innerText = ""   
+
+    } else{
+        showMessage("Invalid value")
+        cashGivenSection.style.display = 'none'   
+        notesSection.style.display = 'none' 
+        
+
+        
+    }
+       
+ });
+
+
+function calculateChange(amountToBeReturned) {
+    
+    for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]); 
+        
+        amountToBeReturned %= availableNotes[i];
+       
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+function hideMessage() {
+    message.style.display = "none";
+}
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
+}
